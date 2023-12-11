bb793bcc6e1b8acc4447896818795a135d4e312c
ZRui98 <ciguatera.z.rui@gmail.com>
2023-12-11 14:53:15 -0500

    2023-11

---
+++ 2023/11/p1.py
@@ -0,0 +1,50 @@
+file = open('input.txt', 'r')
+lines = [line.rstrip() for line in file]
+
+
+rows = set()
+cols = set()
+stars = []
+for i, line in enumerate(lines):
+    for j, c in enumerate(line):
+        if c == '#':
+            rows.add(i)
+            cols.add(j)
+            stars.append((i, j))
+
+ans = 0
+for i, s in enumerate(stars):
+    for j in range(i + 1, len(stars)):
+        destY, destX = s
+        currY, currX = stars[j]
+        distY, distX = 0, 0
+        while destX != currX or destY != currY:
+            if currY < destY:
+                currY += 1 
+                if currY in rows:
+                    distY += 2
+                else:
+                    distY += 1 
+            elif currY > destY:
+                currY -= 1
+                if currY not in rows:
+                    distY += 2
+                else:
+                    distY += 1
+
+            if currX < destX:
+                currX += 1 
+                if currX not in cols:
+                    distX += 2
+                else:
+                    distX += 1 
+            elif currX > destX:
+                currX -= 1
+                if currX not in cols:
+                    distX += 2
+                else:
+                    distX += 1
+        dist = distY + distX
+        ans += dist
+        # print(s, stars[j], dist, distX, distY, currY, currX)
+print(ans)
