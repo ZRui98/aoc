e89cda2cb281efa1e7d3b64f1d9705d8b15d8910
Rui Zhu <ciguatera.z.rui@gmail.com>
2025-12-02 09:33:33 -0500

    add comment

--- 2025/01/p2.py
+++ 2025/01/p2.py
@@ -16,6 +16,7 @@ for k in lines:
         pos += count
     if (pos < 0 or pos > 99):
         pos = pos % 100
+        # if original pos = 0, don't increment 0 count. Similarly, if pos = 0, also don't count it since it's done elsewhere
         if opos != 0 and pos != 0:
             t_count += 1
     if pos == 0:
